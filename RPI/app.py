48b1e8f2d887ea350ee75d79208ccf7892359cb9
Lexine Langford <tanuki360@hotmail.com>
2017-09-02 19:02:16 +0700

    Crate pi script to listen firebase

--- RPI/app.py
+++ RPI/app.py
@@ -1,58 +1,16 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+#!/usr/bin/env python3
+
 import RPi.GPIO as GPIO
-#GPIO.setmode(GPIO.BCM)
+from time import sleep
+from firebase import firebase
+import json
 
-# 8 Channel
+firebase = firebase.FirebaseApplication('https://rpi-iot-homeappliance.firebaseio.com', None)
+controls = firebase.get('/devicesID', None)
+while True:
 
-#INIT STATE
-pins = {
-  ## PIN { ID,name, state (default off) }
-   17 : {'id':'0','name' : 'Light 1     (17)', 'state' : GPIO.LOW},
-   18 : {'id':'1','name' : 'Light 2     (18)', 'state' : GPIO.LOW},
-   27 : {'id':'2','name' : 'Light 3     (27)', 'state' : GPIO.LOW},
-   22 : {'id':'3','name' : 'Light 4     (22)', 'state' : GPIO.LOW},
-   23 : {'id':'4','name' : 'Light 5     (23)', 'state' : GPIO.LOW},
-   24 : {'id':'5','name' : 'Light 6     (24)', 'state' : GPIO.LOW},
-   10 : {'id':'6','name' : 'Light 7     (10)', 'state' : GPIO.LOW},
-   9 :  {'id':'7','name' : 'Light 8     (09)', 'state' : GPIO.LOW}
+	s1 = firebase.get('/devicesID','1/state')
+	s2 = firebase.get('/deviceID','2/state')
+	s3 = firebase.get('/deviceID','3/state')
 
-   }
-
-states = {
-    'on'  : {GPIO.HIGH},
-    'off' : {GPIO.LOW}
-   }
-
-
-
-
-
-
-
-@app.route("/")
-def main():
-
-#   for pin in pins:
-#      pins[pin]['state'] = GPIO.input(pin)
-
-   templateData = {
-#      'pins' : pins
-     }
-
-   return render_template('main.html', **templateData)
-
-
-@app.route("/control/<device_ID>/<action>")
-def state_action(device_ID, action):
-
-   #   changePin = int(changePin)
-   #  deviceName = pins[changePin]['name']
-
-
-  #STATE CHANGE
-   templateData = {
-  #    'pins' : pins
-   }
-
-   return render_template('main.html', **templateData)
+	sleep(2)
